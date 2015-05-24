e934cfebe9fa15c9087ceae31e6fff4ad2f79f71
Edgar Cruz <edgarxene@gmail.com>
2015-05-24 00:34:50 -0500

    Commit inicial

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,62 @@
+@extends('app')
+
+@section('content')
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<div class="panel panel-default">
+				<div class="panel-heading">Login</div>
+				<div class="panel-body">
+					@if (count($errors) > 0)
+						<div class="alert alert-danger">
+							<strong>Whoops!</strong> There were some problems with your input.<br><br>
+							<ul>
+								@foreach ($errors->all() as $error)
+									<li>{{ $error }}</li>
+								@endforeach
+							</ul>
+						</div>
+					@endif
+
+
+					{!! Form::open(array('url' => 'auth/login', 'role' => 'form', 'class' => 'form-horizontal')) !!}
+						<div class="form-group">
+							{!! Form::label('email','Email:', ['class' => 'col-md-4 control-label']) !!}
+							<div class="col-md-4 control-label">
+								{!! Form::email('email', old('email'),
+									['class' => 'form-control']) 
+								!!}
+							</div>
+						</div>
+
+						<div class="form-group">
+							{!! Form::label('password','Password:', ['class' => 'col-md-4 control-label']) !!}
+							<div class="col-md-6">
+								{!! Form::password('password', ['class' => 'form-control']) !!}
+							</div>
+						</div>
+
+						<div class="form-group">
+							<div class="col-md-6 col-md-offset-4">
+								<div class="checkbox">
+									<label>
+										<input type="checkbox" name="remember"> Remember Me
+									</label>
+								</div>
+							</div>
+						</div>
+
+						<div class="form-group">
+							<div class="col-md-6 col-md-offset-4">
+								<button type="submit" class="btn btn-primary">Login</button>
+
+								<a class="btn btn-link" href="{{ url('/password/email') }}">Forgot Your Password?</a>
+							</div>
+						</div>
+					{!! Form::close() !!}				
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
